f03033a86429faa571f59961e10e6faa46a3d8e2
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-05-30 01:38:37 +0500

    Add ERC721 NFT

--- ERC721.sol
+++ ERC721.sol
@@ -3,13 +3,40 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract ERC721NFT is ERC721, Ownable {
+contract ERC721NFT is ERC721, ERC721URIStorage, Ownable {
 
-    constructor(address initialOwner) ERC721("AHMX", "AX") Ownable(initialOwner) {}
+    constructor(address initialOwner)
+        ERC721("AHMX", "AX")
+        Ownable(initialOwner)
+    {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
 }

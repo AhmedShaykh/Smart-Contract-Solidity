a94cea06ad7abae32253b54219207302ac8d9536
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-06-01 22:04:34 +0500

    Add ERC20 Token

--- ERC20.sol
+++ ERC20.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ADXToken is ERC20 {
+contract ERC20Token is ERC20 {
 
-    constructor(uint256 initialSupply) ERC20("ACID X COIN", "ADX") {
+    constructor(uint256 initialSupply) ERC20("ACID X COIN", "AXC") {
         _mint(msg.sender, initialSupply);
     }
 
-    function decimals() public pure override returns(uint8) {
-        return 8;
+    function decimals() public pure override returns (uint8) {
+        return 18; // 1000000000000000000 = 1 AXC Token
     }
 
 }
